95b8a3ac93ff2ef4935884a97b85c22eab5ce264
Gorimaaa <juba.belhocine@outlook.fr>
2025-02-27 17:57:50 +0100

    Some fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Post from "./pages/Post";
@@ -8,13 +8,15 @@ import Login from './pages/Login';
 import { AuthContext } from './helpers/AuthContext';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+
 function App() {
   const backendUrl = process.env.REACT_APP_BACKEND_URL;
   console.log("Backend URL:", backendUrl); // Debugging
   const [authState, setAuthState] = useState({
-     username: "",
-     id: 0, 
-     status: false});
+    username: "",
+    id: 0,
+    status: false
+  });
 
   useEffect(() => {
     axios.get(backendUrl + '/auth/auth', {
@@ -22,11 +24,11 @@ function App() {
         accessToken: localStorage.getItem('accessToken')
       }
     }).then((response) => {
-      if(response.data.error){
+      if (response.data.error) {
         setAuthState({
           ...authState, status: false
         });
-      }else{
+      } else {
         setAuthState({
           username: response.data.username,
           id: response.data.id,
@@ -38,37 +40,37 @@ function App() {
 
   const logout = () => {
     localStorage.removeItem('accessToken');
-    setAuthState({username: "", id: 0, status: false});
-
+    setAuthState({ username: "", id: 0, status: false });
   }
 
   return (
     <div className="App">
-      <AuthContext.Provider value={{authState, setAuthState}}>
-      <Router>
-        <div className="navbar">
-          <div className="links">
-        <Link to="/">Home page</Link>
-        <Link to="/createpost">Create A Post</Link>
-        {(!authState.status) && (
-        <><Link to="/login">Login</Link>
-        <Link to="/registration">Registration</Link>
-        </>
-        )}
-        </div>
-          <div className='loggedInContainer'>
-            <h1>{authState.username}</h1> 
-            {authState.status && <button onClick={logout}>Logout</button>}
+      <AuthContext.Provider value={{ authState, setAuthState }}>
+        <Router>
+          <div className="navbar">
+            <div className="links">
+              <Link to="/">Home page</Link>
+              <Link to="/createpost">Create A Post</Link>
+            </div>
+            <div className='loggedInContainer'>
+              {!authState.status && (
+                <>
+                  <Link to="/login">Login</Link>
+                  <Link to="/registration">Registration</Link>
+                </>
+              )}
+              <h1>{authState.username}</h1>
+              {authState.status && <button onClick={logout}>Logout</button>}
+            </div>
           </div>
-        </div>
-        <Routes>
-          <Route path="/" element={< Home />}/>
-          <Route path="/createpost" element={< CreatePost />}/>
-          <Route path="/post/:id" element={< Post />}/>
-          <Route path="/registration" element={< Registration />}/>
-          <Route path="/login" element={< Login />}/>
-        </Routes>
-      </Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/createpost" element={<CreatePost />} />
+            <Route path="/post/:id" element={<Post />} />
+            <Route path="/registration" element={<Registration />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
       </AuthContext.Provider>
     </div>
   );
